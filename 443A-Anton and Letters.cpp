8d607311e976374e7bacb839337363af08348ae8
Aditya Walia <42476689+Walia8416@users.noreply.github.com>
2021-02-12 03:29:18 +0000

    Add files via upload

---
+++ 443A-Anton and Letters.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string set;
+int len;
+int j=0, count=0;
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	    freopen("input.txt", "r", stdin);
+	    freopen("output.txt", "w", stdout);
+	#endif
+	
+	getline(cin, set);
+	len = set.length();
+		
+	char letters[len/3];
+	for (int i=1; i<len-1; i+=3)
+	{	
+		letters[j] = set[i];
+		j++;
+	}
+
+	for (int i=0; i<(len/3); i++)
+	{
+		for (int j=i+1; j<(len/3); j++)
+		{
+			if (letters[i] == letters[j])
+			{
+				letters[i] = '\0';
+				count+=1;
+			}
+		}
+	}
+
+	cout << (len/3)-count << endl;
+
+
+}
